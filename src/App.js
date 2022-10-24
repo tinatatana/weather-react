3ef467d17cd9f86ee868a94cd3848460c2a139b0
tinatatana <109757952+tinatatana@users.noreply.github.com>
2022-10-24 21:55:44 +0200

    added source link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ export default function App() {
   return (
     <div className="App">
       <Weather />
+      <a href="https://github.com/tinatatana/weather-react">
+        Open-sourse code
+      </a>{" "}
+      by Kristina Poliukhovych
     </div>
   );
 }
